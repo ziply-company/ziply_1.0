b4c7277c2da944e6a9e030ff23b535a0ce924295
Oleksandr Podynihlazov <x00215403@mytudublin.ie>
2025-06-21 03:26:22 +0100

    chores(ziply):make init conf for postgres,redis,celery,uvicorn,docker,django,pre-commit.

---
+++ ziply/celery.py
@@ -0,0 +1,8 @@
+import os
+
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ziply.settings")
+app = Celery("ziply")
+app.config_from_object("django.conf:settings", namespace="SELERY")
+app.autodiscover_tasks()
